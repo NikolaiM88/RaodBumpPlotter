1ea18160b33a41c5b1489bd60dff8a0e41e7bbaf
NikolaiM88 <NikolaiM88@hotmail.com>
2016-05-04 16:00:54 +0200

    Sensor moved to seperate class

--- app/src/main/java/com/example/nikolai/roadbumpplotter/MapsActivity.java
+++ app/src/main/java/com/example/nikolai/roadbumpplotter/MapsActivity.java
@@ -1,9 +1,5 @@
 package com.example.nikolai.roadbumpplotter;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -12,12 +8,13 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, SensorEventListener, LocationListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
-    private SensorManager sensorManager;
-    private float threshold = 5;
+    private Location location;
 
 
     @Override
@@ -28,12 +25,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        sensorManager.registerListener(this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_NORMAL);
+        Sensor sensor = new Sensor();
 
     }
 
@@ -53,23 +45,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            if (event.values[1] > threshold) {
-
-
-            }
-
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    @Override
     public void onLocationChanged(Location location) {
-
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(location.getLatitude(), location.getLongitude())));
     }
 }
