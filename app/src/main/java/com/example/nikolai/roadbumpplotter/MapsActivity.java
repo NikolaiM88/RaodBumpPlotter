f4391c3848d6dd9ab5c6f04fc09a4f59c1519b03
NikolaiM88 <NikolaiM88@hotmail.com>
2016-05-08 15:30:33 +0200

    should make a marker if bump is big enough.

--- app/src/main/java/com/example/nikolai/roadbumpplotter/MapsActivity.java
+++ app/src/main/java/com/example/nikolai/roadbumpplotter/MapsActivity.java
@@ -26,6 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         Sensor sensor = new Sensor();
+        sensor.magThread.start();
 
     }
 
